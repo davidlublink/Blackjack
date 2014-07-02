74f6ef0a827eceae306873f33cd80209a819718b
David Lublink <david.lublink@omnivigil.com>
2014-07-02 10:09:23 -0400

    * Started implementing Illustrious 18

--- Players/HiLo.php
+++ Players/HiLo.php
@@ -8,8 +8,11 @@ Class BlackJackPlayer_HiLo extends BlackJackPlayer
 
      protected $count = 0 ;
 
+     private $game ;
+
      public function getBet( BlackJackGame $game ) /*{{{*/
      {
+          $this->game = $game ;
 //          if ( $this->count < -5 ) return 0 ;//  throw new exception("Player left the table because of bad count!");
 
           $decks = $game->getCardsRemaining() / 54 ;
@@ -53,12 +56,46 @@ Class BlackJackPlayer_HiLo extends BlackJackPlayer
 
      public function wantInsurance( $game, $cost )/*{{{*/
      {
-          return $this->getTrueCount( $game ) > 1;
+          return $this->getTrueCount( $game ) >= 3;
      }/*}}}*/
 
-     public function leaveGame()
+     public function deal( BlackJackHand $dealerHand, array $others, BlackJackHand $me ) /*{{{*/
      {
-          return $this->count < -5; 
-     }
+          if ( $this->game === null ) return parent::deal($dealerHand, $others, $me ); 
+
+          $dealer = $dealerHand->getShown();
+
+          $i = 3;
+
+          $count = $this->getTrueCount($this->game) ;
+
+          while ( $i-- > 0  )
+          {
+               list( $soft, $value ) = $me->getValue(); 
+
+               // Illustrious 2
+               if ( $count >= 0 && $value === 16 && in_array( $dealer, array( '10', 'J', 'Q', 'K', 'A' ) ) ) 
+               {
+                    return ;
+               }
+
+               // Illustrious 2
+               if ( $count >= 4 && $value === 15 && in_array( $dealer, array('10', 'J', 'Q', 'K', 'A' ) ) )
+               {
+                    return ;
+               }
+
+               // Illustrious 3
+               if ( $count >= 5 && $value === 20 && $this->isSplitAllowed() && in_array( $dealer, array('5') ) )
+               {
+                    
+                    return ;
+               }
+
+          }
+
+
+          return parent::deal( $dealerHand, $others, $me );
+     } /*}}}*/ 
 
 }
